7c14285d3a0d20923ec0efcf8ce0bec02810f648
torsaft <knudsen3030@hotmail.no>
2023-01-25 12:03:55 +0100

    just removed a test

--- model/parts/policy_test.go
+++ model/parts/policy_test.go
@@ -1,7 +1,6 @@
 package policy
 
 import (
-	. "go-incentive-simulation/model/parts/types"
 	"testing"
 
 	"gotest.tools/assert"
@@ -15,16 +14,4 @@ func TestResponisbleNodes(t *testing.T) {
 	assert.Equal(t, len(values), 4)
 }
 
-func TestGetNodeById(t *testing.T) {
-	n := Network{}
-	n.Bin = 8
-	n.Bits = 16
 
-	n1 := Node{}
-	n1.Id = 111
-
-	n2 := Node{}
-	n2.Id = 222
-
-	//GetNodeById(111)
-}
