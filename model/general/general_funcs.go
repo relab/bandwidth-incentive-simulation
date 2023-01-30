289ab48b49c146e2f20a5b778932296a38ebc065
torsaft <knudsen3030@hotmail.no>
2023-01-30 10:42:29 +0100

    moved binarysearh to general folder

--- model/general/general_funcs.go
+++ model/general/general_funcs.go
@@ -28,3 +28,41 @@ func Contains[T comparable](elems []T, value T) bool {
 	}
 	return false
 }
+
+func BinarySearchClosest(arr []int, target int, n int) []int {
+	left, right := 0, len(arr)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if arr[mid] == target {
+			return findClosest(arr, target, mid, n)
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return findClosest(arr, target, left, n)
+}
+
+func findClosest(arr []int, target int, index int, n int) []int {
+	result := make([]int, 0, n)
+	left, right := index-1, index+1
+	for left >= 0 && right < len(arr) && len(result) < n {
+		if target-arr[left] < arr[right]-target {
+			result = append(result, arr[left])
+			left--
+		} else {
+			result = append(result, arr[right])
+			right++
+		}
+	}
+	for left >= 0 && len(result) < n {
+		result = append(result, arr[left])
+		left--
+	}
+	for right < len(arr) && len(result) < n {
+		result = append(result, arr[right])
+		right++
+	}
+	return result
+}
