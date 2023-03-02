530a90ba5de2ad46ee34ac6a7fae3d040a462986
filigott <filigott@lyse.net>
2023-03-02 11:46:09 +0100

    fix test

--- model/parts/update/update_functions_test.go
+++ model/parts/update/update_functions_test.go
@@ -58,7 +58,7 @@ func TestFailedRequestsAccess(t *testing.T) {
 func TestUpdateOriginatorIndex(t *testing.T) {
 	state := MakeInitialState(path)
 	UpdateOriginatorIndex(&state)
-	if state.OriginatorIndex >= Constants.GetOriginators() {
+	if int(state.OriginatorIndex) >= Constants.GetOriginators() {
 		if state.OriginatorIndex != 0 {
 			t.Errorf("UpdateOriginatorIndex() failed, expected < %d, got %d", 0, state.OriginatorIndex)
 		}
