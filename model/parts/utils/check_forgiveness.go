2b9f18e6292d0e9ebdd47598dc7766eaf773efc7
filigott <filigott@lyse.net>
2023-03-16 21:01:49 +0100

    Changed forgiveness to reference a change of epoch/new second, instead of an interval of timesteps.

--- model/parts/utils/check_forgiveness.go
+++ model/parts/utils/check_forgiveness.go
@@ -7,7 +7,8 @@ import (
 )
 
 func CheckForgiveness(edgeData types.EdgeAttrs, firstNodeId int, secondNodeId int, graph *types.Graph, request types.Request) (int, bool) {
-	passedTime := (request.TimeStep - edgeData.Last) / constants.GetRequestsPerSecond()
+	//passedTime := (request.TimeStep - edgeData.Last) / constants.GetRequestsPerSecond()
+	passedTime := request.Epoke - edgeData.EpokeLastForgiven
 	if passedTime > 0 {
 		refreshRate := constants.GetRefreshRate()
 		if constants.IsAdjustableThreshold() {
@@ -19,7 +20,8 @@ func CheckForgiveness(edgeData types.EdgeAttrs, firstNodeId int, secondNodeId in
 		if newEdgeData.A2B < 0 {
 			newEdgeData.A2B = 0
 		}
-		newEdgeData.Last = request.TimeStep
+		//newEdgeData.Last = request.Epoke
+		newEdgeData.EpokeLastForgiven = request.Epoke
 		graph.SetEdgeData(firstNodeId, secondNodeId, newEdgeData)
 		return newEdgeData.A2B, true
 	}
