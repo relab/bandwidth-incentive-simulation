82a5f644aed6ab6fd672bc61986a36ccda681c29
torsaft <knudsen3030@hotmail.no>
2023-03-23 12:01:47 +0100

    confOptions changes happen first then switch statment determine experiment

--- config/choos_experiment.go
+++ config/choos_experiment.go
@@ -28,36 +28,45 @@ func ReadYamlFile() Yml {
 }
 
 func SetExperiment(experiment Yml) {
-	//ConfOptions(experiment.ConfigOptions)
+	ConfOptions(experiment.ConfOptions)
 
 	switch experiment.Experiment.ExperimentName {
 	case "omega":
 		fmt.Println("omega experiment chose")
 		OmegaExperiment()
+
 	case "k20p20":
 		fmt.Println("bucket size 20 and 20% originators experiment chose")
 		BucketSize20And20pOriginators()
+
 	case "k20p100":
 		fmt.Println("bucket size 20 and 100% originators experiment chose")
 		BucketSize20And100pOriginators()
+
 	case "k16p20":
 		fmt.Println("bucket size 16 and 20% originators experiment chose")
 		BucketSize16And20pOriginators()
+
 	case "k16p100":
 		fmt.Println("bucket size 16 and 100% originators experiment chose")
 		BucketSize16And100pOriginators()
+
 	case "k8p20":
 		fmt.Println("bucket size 8 and 20% originators experiment chose")
 		BucketSize8And20pOriginators()
+
 	case "k8p100":
 		fmt.Println("bucket size 8 and 100% originators experiment chose")
 		BucketSize8And100pOriginators()
+
 	case "k4p20":
 		fmt.Println("bucket size 4 and 20% originators experiment chose")
 		BucketSize4And20pOriginators()
+
 	case "k4p100":
 		fmt.Println("bucket size 4 and 100% originators experiment chose")
 		BucketSize4And100pOriginators()
+		
 	case "custom":
 		fmt.Println("custom experiment chose")
 		CustomExperiment(experiment.Custom)
