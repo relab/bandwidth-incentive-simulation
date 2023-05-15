6c0b79097fb556d04729c3607546220c1afc4980
Leander Jehl <leanderjehl@yahoo.no>
2023-05-15 12:52:17 +0200

    command line flags for network generation

--- data/generate_data.go
+++ data/generate_data.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"go-incentive-simulation/config"
 	"go-incentive-simulation/model/parts/types"
 	"math/rand"
 )
 
 func main() {
-	binSize := config.GetBinSize()
-	bits := config.GetBits()
-	networkSize := config.GetNetworkSize()
-	rand.Seed(config.GetRandomSeed())
-	network := types.Network{Bits: bits, Bin: binSize}
-	network.Generate(networkSize)
-	filename := fmt.Sprintf("nodes_data_%d_%d.txt", binSize, networkSize)
+
+	binSize := flag.Int("binSize", 16, "Number of nodes in each address table bin, k in Kademlia")
+
+	bits := flag.Int("bits", 16, "address length in bits")
+	networkSize := flag.Int("N", 10000, "network size")
+	rSeed := flag.Int("rSeed", -1, "random Seed")
+	id := flag.String("id", "", "an id")
+
+	flag.Parse()
+
+	if *rSeed != -1 {
+		rand.Seed(int64(*rSeed))
+	}
+	network := types.Network{Bits: *bits, Bin: *binSize}
+	network.Generate(*networkSize)
+	filename := fmt.Sprintf("nodes_data_%d_%d.txt", *binSize, *networkSize)
+	if *id != "" {
+		filename = fmt.Sprintf("nodes_data_%d_%d_%s.txt", *binSize, *networkSize, *id)
+	}
 	err := network.Dump(filename)
 	if err != nil {
 		return
