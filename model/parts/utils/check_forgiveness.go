f2fd3414bd293c46ce6626c42cd71bb59ca9451c
Leander Jehl <leanderjehl@yahoo.no>
2023-05-28 07:35:03 +0200

    new variant of ajusted forgiveness that reduces forgiveness more drastically.

--- model/parts/utils/check_forgiveness.go
+++ model/parts/utils/check_forgiveness.go
@@ -12,7 +12,9 @@ func CheckForgiveness(edgeData types.EdgeAttrs, firstNodeId types.NodeId, second
 	if passedTime > 0 {
 		refreshRate := config.GetRefreshRate()
 		if config.IsAdjustableThreshold() {
-			refreshRate = int(math.Ceil(float64(edgeData.Threshold / 2)))
+			// TODO: The exponent (6) here should be a configuration parameter.
+			refreshRate = GetAdjustedRefreshrate(edgeData.Threshold, config.GetThreshold(), config.GetRefreshRate(), 6)
+			//refreshRate = int(math.Ceil(float64(edgeData.Threshold / 2)))
 		}
 		removedDeptAmount := passedTime * refreshRate
 		newEdgeData := edgeData
@@ -25,4 +27,9 @@ func CheckForgiveness(edgeData types.EdgeAttrs, firstNodeId types.NodeId, second
 		return newEdgeData.A2B, true
 	}
 	return edgeData.A2B, false
+}
+
+func GetAdjustedRefreshrate(adjustedThreshold, threshold, refreshRate, power int) int {
+	ratio := float64(adjustedThreshold) / float64(threshold)
+	return int(math.Ceil(float64(refreshRate) * math.Pow(ratio, float64(power))))
 }
