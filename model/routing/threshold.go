741bbd8a7b5bff083dfeac27ae117093418fa53c
vhpl-uis <heidaripour.l.vahid@uis.no>
2023-07-11 13:38:00 +0200

    make checkForgiveness a method on the edge

--- model/routing/threshold.go
+++ model/routing/threshold.go
@@ -10,15 +10,16 @@ func IsThresholdFailed(firstNodeId types.NodeId, secondNodeId types.NodeId, grap
 	if !config.GetThresholdEnabled() {
 		return false
 	}
-
-	edgeDataFirst := graph.GetEdgeData(firstNodeId, secondNodeId)
-	p2pFirst := edgeDataFirst.A2B
-	edgeDataSecond := graph.GetEdgeData(secondNodeId, firstNodeId)
-	p2pSecond := edgeDataSecond.A2B
+	var edgeWrapper EdgeWrapper
+	edgeDataFirst := graph.GetEdge(firstNodeId, secondNodeId)
+	p2pFirst := edgeDataFirst.Attrs.A2B
+	edgeDataSecond := graph.GetEdge(secondNodeId, firstNodeId)
+	p2pSecond := edgeDataSecond.Attrs.A2B
+	edgeWrapper.Edge = *edgeDataFirst
 
 	threshold := config.GetThreshold()
 	if config.IsAdjustableThreshold() {
-		threshold = edgeDataFirst.Threshold
+		threshold = edgeDataFirst.Attrs.Threshold
 	}
 
 	peerPriceChunk := utils.PeerPriceChunk(secondNodeId, request.ChunkId)
@@ -30,7 +31,7 @@ func IsThresholdFailed(firstNodeId types.NodeId, secondNodeId types.NodeId, grap
 	//fmt.Printf("price: %d = p2pFirst: %d - p2pSecond: %d + PeerPriceChunk: %d \n", price, p2pFirst, p2pSecond, peerPriceChunk)
 
 	if price > threshold && config.IsForgivenessEnabled() {
-		newP2pFirst, forgiven := CheckForgiveness(edgeDataFirst, firstNodeId, secondNodeId, graph, request)
+		newP2pFirst, forgiven := edgeWrapper.CheckForgiveness(graph, request)
 		if forgiven {
 			price = newP2pFirst - p2pSecond + peerPriceChunk
 		}
